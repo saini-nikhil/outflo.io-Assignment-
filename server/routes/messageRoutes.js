214da93445cf3fc97b831b91627e799f1fe4c776
Nikhilsaini505 <1nikhilsaini234@gmail.com>
2025-04-17 10:28:27 +0530

    add favicon icons

--- server/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generatePersonalizedMessage } = require('../controllers/messageController');
-
-// LinkedIn message generation route
-router.post('/personalized-message', generatePersonalizedMessage);
-
-module.exports = router;
